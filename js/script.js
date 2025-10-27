b136eebc5a96623700d146fe797239b2ac3a75e4
simon-wdev <214550778+simon-wdev@users.noreply.github.com>
2025-10-27 16:22:43 +0100

    Make adjustable canvas
    The canvas is now adjustable via prompt. Added event listeners for color trail.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,45 @@
-const canvasBox = document.querySelector(".canvasBox")
+const canvasBox = document.querySelector(".canvasBox");
+const canvasItem = document.querySelector(".canvasItem")
+
+
+let grid = prompt("Whats your grid size?");
+let columns = grid;
+let rows = grid;
+getWidth(rows, columns);
+setCanvas(rows, columns);
+
+
+function getWidth(rows, columns){
+    canvasBox.style.width = `${columns*25}px`;
+    canvasBox.style.height = `${rows*25}px`;
+}
 
 function setCanvas(rows, columns){
     let rowCount = 0;
-    while (rowCount < rows){
-        for (let i = 0; i < columns; i++){
+    while (rowCount < rows){    //mcreates the rows
+        for (let i = 0; i < columns; i++){  //creates the columns
             const canvasItem = document.createElement("div");
             canvasItem.classList.add("canvasItem")
             canvasBox.appendChild(canvasItem);
+
+                canvasItem.addEventListener('mouseenter', () =>{
+                    canvasItem.style.backgroundColor = "red";
+                });
+
+                canvasItem.addEventListener('mouseleave', () =>{
+                    setTimeout(()=>{
+                        canvasItem.style.backgroundColor = "";}, "800");
+                    
+                });
         }
         rowCount++;
     }   
 }
 
-setCanvas(16, 16);
+
+
+
+
+
+
+
