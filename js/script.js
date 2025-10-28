4d8fdf156625310844100b451f419873b80083b3
simonw-dev <214550778+simon-wdev@users.noreply.github.com>
2025-10-28 09:59:02 +0100

    Make random colors for hovering
    When hovered with the mouse, divs background color will now be a random value out of an array.

--- js/script.js
+++ js/script.js
@@ -1,16 +1,26 @@
 const canvasBox = document.querySelector(".canvasBox");
 const canvasItem = document.querySelector(".canvasItem")
 
-
-let grid = prompt("Whats your grid size?");
+let grid = 16;
 let columns = grid;
 let rows = grid;
-getWidth(rows, columns);
-setCanvas(rows, columns);
+setSize();
 
+
+function setSize(){
+    if (grid>100){
+        alert("100 is max! Grid will be set to max.");
+        setCanvas(100, 100);
+    }else{
+        let columns = grid;
+        let rows = grid;
+        getWidth(rows, columns);
+        setCanvas(rows, columns);
+    }
+}
 
 function getWidth(rows, columns){
-    canvasBox.style.width = `${columns*25}px`;
+    canvasBox.style.width = `${columns*25}px`; //box width/height multiplied by number of columns
     canvasBox.style.height = `${rows*25}px`;
 }
 
@@ -22,14 +32,11 @@ function setCanvas(rows, columns){
             canvasItem.classList.add("canvasItem")
             canvasBox.appendChild(canvasItem);
 
-                canvasItem.addEventListener('mouseenter', () =>{
-                    canvasItem.style.backgroundColor = "red";
-                });
-
-                canvasItem.addEventListener('mouseleave', () =>{
-                    setTimeout(()=>{
-                        canvasItem.style.backgroundColor = "";}, "800");
-                    
+                let color = ["red", "blanchedalmond", "lightgreen", "darkorchid", "darkseagreen", "firebrick", "lightcoral", "orange", "skyblue"];
+                let randomColor = color[Math.floor(Math.random()* 9)]
+                console.log(randomColor)
+                canvasItem.addEventListener('mouseover', () =>{          
+                    canvasItem.style.backgroundColor = `${randomColor}`;
                 });
         }
         rowCount++;
